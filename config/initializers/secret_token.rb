b6326ca2986de5404ab3c280d4697ff5d0ad71f9
Jahde <jahde.hiywan@gmail.com>
2014-10-04 10:57:25 -0400

    My initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobBoard::Application.config.secret_key_base = 'afa1e09779ad3c137a63854c3ce489e214a0b86657bfd0ac71eba3757c303820c82717524cceb3d99063856466957f85687c0bd9df7a20ecc4c3d73dbde5c664'
